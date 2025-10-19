350bf171d655209311c1c90c8a1345defbc97ad2
atletisimo <37110896+atletisimo@users.noreply.github.com>
2025-10-19 13:35:15 +0200

    Update perfectNumber.py

--- perfectNumber.py
+++ perfectNumber.py
@@ -5,3 +5,19 @@
 28 → делители: 1, 2, 4, 7, 14 → 1 + 2 + 4 + 7 + 14 = 28 ✅
 
 10 → делители: 1, 2, 5 → 1 + 2 + 5 = 8 ❌ (не е совршен)
+
+def is_perfect(n):
+ sum_deliteli=0
+ for i in range(1,n):
+     if n%i==0:
+      sum_deliteli=sum_deliteli+i
+ return sum_deliteli==n # kje vrati true/false dali zbitoy na deliteli e ednakov na samiot toj broj
+
+
+n=int(input("Vnesi broj: "))
+if(is_perfect(n)):
+    print(f"{n} e perfecten broj") #f пред наводниците значи “formatted string literal” — овозможува да вметнеш променливи директно во текстот со {}.
+  #  print(n, "e perfecten broj")
+else:
+    print(f"{n} ne e perfecten broj")
+#   print(n, "ne e perfecten broj")
